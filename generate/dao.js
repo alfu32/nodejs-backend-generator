328f2b4761fb4667adf74596851fa04c36321d2b
Glitch (glitch-hello-node) <none>
2021-12-23 09:41:29 +0000

    🐫🍞 Checkpoint ./generate/dao.js:38339194/360 ./generate/model.json.js:38339194/6

--- generate/dao.js
+++ generate/dao.js
@@ -5,11 +5,19 @@ const meta=Object.keys(model)
   n => {
     const def=model[n];
     const table=n.toUppercase();
-    const columns=def.
+    const columns=Object.keys(def);
+    const pk=columns[0];
+    const fks=columns.filter(k=>k.match(/_id$/gi))
     const daoMetadata={
       moduleName:`${n}Dao`,
       sql:{
-        drop:`DROP TABLE IF NOT EXISTS ${table}`
+        pk,
+        fks,
+        drop:`DROP TABLE IF NOT EXISTS ${table}`,
+        create:`CREATE TABLE I NOT EXIST ${table}`,
+        insert:`INSERT INTO ${table}(
+        ${columns.join(',')}
+        ) VALUES ${columns.map(n=>`@${n}`).join(',')}`,
       }
     }
   }
