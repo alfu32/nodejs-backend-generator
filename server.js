e1f2968bed37dd5ab0c9384b6f0a4d478c518eec
Glitch (glitch-hello-node) <none>
2021-12-23 08:45:05 +0000

    🎹💋 Checkpoint ./package.json:38339194/1496 ./server.js:38339194/556

--- server.js
+++ server.js
@@ -10,30 +10,15 @@ const fastify = require("fastify")({
   // Set this to true for detailed logging:
   logger: false
 });
+const express = require('express');
+const app = express();
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-fastify.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
-
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
-
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
-});
-
-fastify.get('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
   res.send({
     host: `${process.env.PROJECT_DOMAIN}.glitch.me`,
     port:process.env.PORT
@@ -41,11 +26,10 @@ fastify.get('/',(req,res,next)=>{
 });
 
 // Run the server and report out to the lo
-fastify.listen(process.env.PORT, function(err, address) {
+app.listen(process.env.PORT, function(err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
   console.log(`Your app is listening on ${address} ${process.env.PORT}`);
-  fastify.log.info(`server listening on ${address}`);
 });
