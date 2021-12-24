0ade52b7bed67178459e771a73bf14b2840ca33a
Glitch (glitch-hello-node) <none>
2021-12-24 11:41:43 +0000

    🍀💽 Checkpoint ./generate/dao.js:38339194/2348

--- generate/dao.js
+++ generate/dao.js
@@ -23,65 +23,77 @@ function createDefaultTableDefinition(tableName){
   return cfg;
 }
 
-let meta= definedTables.map(modelMapper);
-meta={...meta,...undefinedTables.map(modelMapper)}
-function modelMapper(n){
-    const def=model[n];
-    const table=n.toUpperCase();
-    const columns=Object.keys(def);
-    const pk=columns[0];
-    const fks=columns.filter(k=>k.match(/_id$/gi) && k!==pk);
-    const cols=columns.filter(k => fks.indexOf(k)==-1 && k!==pk)
-    
-    const daoMetadata={
-      moduleName:`${n}Dao`,
-      sql:{
-        pk,
-        fks,
-        fk:fks.join(','),
-        cols:cols.join(','),
-        drop:`DROP TABLE IF NOT EXISTS ${table}`,
-        create:`CREATE TABLE IF NOT EXIST ${table}(
-          ${pk} VARCHAR PRIMARY KEY DEFAULT (${uuid}),
-          ${
-            fks.concat(cols).map(
-              k => `${k} VARCHAR`
-            ).join(',\n')
+let meta= definedTables.map(modelMapper(model));
+meta=[...meta,...Object.keys(undefinedTables).map(modelMapper(undefinedTables))]
+function modelMapper(model){
+  return function modelMapper(n){
+      const def=model[n];
+      const table=n.toUpperCase();
+      const columns=Object.keys(def);
+      const pk=columns[0];
+      const fks=columns.filter(k=>k.match(/_id$/gi) && k!==pk);
+      const cols=columns.filter(k => fks.indexOf(k)==-1 && k!==pk)
+
+      const daoMetadata={
+        moduleName:`${n}Dao`,
+        sql:{
+          metadata:{
+            pk,
+            fks,
+            fk:fks.join(','),
+            cols:cols.join(','),
           },
-          ${fks.map(
-            fk => {
-              const ftable = fk.replace(/_id$/gi,'')
-                .toUpperCase();
-              if(! definedTablesIndex[ftable] ){
-                undefinedTables={...undefinedTables,...createDefaultTableDefinition(ftable)}
-              }
-              return `FOREIGN KEY(${fk}) REFERENCES ${ftable}(${fk}) `
-            }
-          ).join(',\n')}
-        )`,
-        insert:`INSERT INTO ${table}(
-        ${fks.concat(cols).join(',')}
-        ) VALUES ${fks.concat(cols).map(n=>`@${n}`).join(',')}`,
-        updateSingle:`UPDATE ${table} SET
-        ${fks.concat(cols).map(k=>`${k}=@${k}`).join(',\n')}
-        WHERE ${pk}=@${pk}`,
-        deleteSingle:`DELETE FROM ${table} 
-        WHERE ${pk}=@${pk}`,
-        getSingle:`SELECT * FROM ${table} 
-        WHERE ${pk}=@${pk}`,
-        getAll:`SELECT * FROM ${table}`,
-        countAll:`SELECT COUNT(*) as records FROM ${table}`,
+          operations:{
+            drop:`DROP TABLE IF NOT EXISTS ${table}`,
+            create:`CREATE TABLE IF NOT EXIST ${table}(
+              ${pk} VARCHAR PRIMARY KEY DEFAULT (${uuid}),
+              ${
+                fks.concat(cols).map(
+                  k => `${k} VARCHAR`
+                ).join(',\n')
+              },
+              ${fks.map(
+                fk => {
+                  const ftable = fk.replace(/_id$/gi,'')
+                    .toUpperCase();
+                  if(! definedTablesIndex[ftable] ){
+                    undefinedTables={...undefinedTables,...createDefaultTableDefinition(ftable)}
+                  }
+                  return `FOREIGN KEY(${fk}) REFERENCES ${ftable}(${fk}) `
+                }
+              ).join(',\n')}
+            )`,
+            insert:`INSERT INTO ${table}(
+            ${fks.concat(cols).join(',')}
+            ) VALUES ${fks.concat(cols).map(n=>`@${n}`).join(',')}`,
+            updateSingle:`UPDATE ${table} SET
+            ${fks.concat(cols).map(k=>`${k}=@${k}`).join(',\n')}
+            WHERE ${pk}=@${pk}`,
+            deleteSingle:`DELETE FROM ${table} 
+            WHERE ${pk}=@${pk}`,
+            getSingle:`SELECT * FROM ${table} 
+            WHERE ${pk}=@${pk}`,
+            getAll:`SELECT * FROM ${table}`,
+            countAll:`SELECT COUNT(*) as records FROM ${table}`,
+          }
         }
-    }
-    fks.forEach(
-      fk => {
-        daoMetadata.sql[`selectBY${fk}`]=`SELECT * FROM ${table} 
-        WHERE ${fk}=@${fk}`;
-        daoMetadata.sql[`countBY${fk}`]=`SELECT COUNT(*) as records FROM ${table} 
-        WHERE ${fk}=@${fk}`
       }
-    );
-    
-    return daoMetadata;
-  }
+      fks.forEach(
+        fk => {
+          daoMetadata.sql.operations[`selectBY${fk}`]=`SELECT * FROM ${table} 
+          WHERE ${fk}=@${fk}`;
+          daoMetadata.sql.operations[`countBY${fk}`]=`SELECT COUNT(*) as records FROM ${table} 
+          WHERE ${fk}=@${fk}`
+        }
+      );
+
+      console.log(daoMetadata);
+      return daoMetadata;
+    }
+}
+
+
 console.log(meta);
+
+
+
